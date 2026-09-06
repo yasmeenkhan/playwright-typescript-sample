3a39f395a1cc9e17c6dc06954d188e7cb8f9e04c
yasmeenkhan <yasmeenkhan9331@gmail.com>
2026-09-06 17:05:21 +0530

    second_installment

---
+++ js-practice-code/01_basics/06_strings.js
@@ -0,0 +1,29 @@
+const name = "Yasmeen"
+const repoCount = 0
+
+// console.log(`My name is ${name} and my repo count is ${repoCount}`);
+
+const something = new String('  My name is yasmeen.   ');
+// console.log(something[0])
+// console.log(something.__proto__)
+
+// console.log(something.length)
+// console.log(something.toUpperCase());
+
+// console.log(something.charAt(2));
+// console.log(something.indexOf('m'));
+
+// const newString = something.substring(0,4)
+// console.log(newString)
+
+// const anotherString = something.substring(-8,4)
+// console.log(anotherString)
+
+// console.log(something.trim())
+
+console.log(something.replace("yasmeen", "something").trim())
+
+
+
+
+
